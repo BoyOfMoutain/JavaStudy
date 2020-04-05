737fcfdc33728633da7e2f91e5578893dc00e0a7
DongBoYe <270150186@qq.com>
2020-04-06 01:54:27 +0800

    Java学习

--- JavaFoundation/src/main/java/com/boye/foundation/AbstractClass.java
+++ JavaFoundation/src/main/java/com/boye/foundation/AbstractClass.java
@@ -13,13 +13,20 @@ public abstract  class AbstractClass {
 class ExtendAbstractClass extends AbstractClass{
 
     public static void main(String[] args) {
-        AbstractClass a = new ExtendAbstractClass();
+        AbstractClass a = new ExtendAbstractClass();//调用抽象类的实现类方法
         a.test();
-      //  AbstractClass.test2();
+
+        AbstractClass b = new AbstractClass() {
+            @Override
+            public void test() {
+                System.out.println("匿名内部类的方法");
+            }
+        };
+        b.test();//调用匿名内部类的方法
     }
 
     @Override
     public void test() {
-        System.out.println("hello world!");
+        System.out.println("子类对抽象类的方法重载!");
     }
 }
